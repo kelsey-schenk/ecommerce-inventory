873e5e8daef966f97c559d4b7974cbefa29b1b0e
kelsey-schenk <kelsey.schenk@att.net>
2022-01-23 18:00:12 -0500

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,14 +39,29 @@ router.get('/:id', (req, res) => {
       ]
     }
   )
+  .then(tagData => res.json(tagData))
+  
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
